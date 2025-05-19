a5aa7cc8f62965bd8b2cbcaf776b7e86e32b1705
Mac Anderson <mac@macanderson.com>
2025-05-19 00:36:41 -0700

    update

--- apps/web/pages/sign-in/[[...index]].tsx
+++ apps/web/pages/sign-in/[[...index]].tsx
@@ -1,5 +1,22 @@
-import { SignIn } from '@clerk/nextjs';
+import { SignIn } from "@clerk/nextjs";
+
+const appearance = {
+  variables: {
+    colorPrimary: "#000",
+    colorTextPrimary: "#000",
+    colorTextSecondary: "#000",
+    colorTextPlaceholder: "#000",
+    colorTextLink: "#000",
+    colorTextLinkHover: "#000",
+  },
+  layout: {
+    logoImage: "/logo.png",
+    logoPlacement: "inside" as "inside",
+    logoWidth: "100px",
+    socialButtonsVariant: "iconButton" as "iconButton",
+  },
+};
 
 export default function SignInPage() {
-  return <SignIn routing="path" path="/sign-in" />;
+  return <SignIn routing="path" path="/sign-in" appearance={appearance} />;
 }
